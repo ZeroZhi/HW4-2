8cbf6b046671cb88d36e430bc15d805e9923ef24
ZerpZhi <zekazilin@gmail.com>
2015-05-22 22:08:31 +0300

    отформатирован код

--- function.php
+++ function.php
@@ -1,43 +1,34 @@
 <?php
 
-function factorial($x) 
- {
-    if ($x==1) 
- {
-	return $x; 
- }
-    else 
- {
-    return $x * factorial($x-1);  
+function factorial($x) {
+   if ($x==1) {
+   return $x; 
+ } else {
+   return $x * factorial($x-1);  
  } 
-    return $x;
+   return $x;
  }
 
 
-function treugolnik($a,$h)
- {
+function treugolnik($a,$h) {
    $s = 0.5*$a*$h;
    return $s;
  }
 
 
-function priamougolnik($a,$b)
- {
+function priamougolnik($a,$b) {
    $s = $a*$b;
    return $s;
  }
 
 
-function krug($r)
- {
+function krug($r) {
    $pi = 3.141592;
    $s = $pi* ($r*$r);
    return $s;
  }
 
-function summa_slov($summa)
- {
-    $z = str_word_count($summa);
-    $summa = $z;
+function summa_slov($summa) {
+    $summa = str_word_count($summa);
     return $summa;
  }
--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 <?php
 
  require "function.php";
+
   $string = file_get_contents('http://source-it.com.ua/teachers/'); 
   $string = strip_tags($string);
-  $sub ='Татьяна Муращенко';
-  $pos = mb_strpos($string, $sub);
-  $cut = substr($string, $pos, 49092);
+     $sub ='Татьяна Муращенко';
+     $pos = mb_strpos($string, $sub);
+     $cut = substr($string, $pos, 49092);
+     $cut = trim ($cut);
+   $znaki = array('.',',','(',')',';',':','+','"');
+     $cut = str_replace($znaki,"",$cut);
 
  echo "Всего слов в док-те - " .summa_slov($cut)."<br>";
-
-  $cut = trim ($cut);
-  $znaki = array('.',',','(',')',';',':','+','"');
-  $cut = str_replace($znaki,"",$cut);
-  $z = explode(' ', $cut);
+ 
+       $z = explode(' ', $cut);
 
  print_r(array_count_values($z));
  
